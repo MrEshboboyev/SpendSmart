ec62ae1f43499a23d56244978952693236a5ad3b
MrEshbobotev <mreshboboyev@gmail.com>
2025-02-22 21:20:44 +0500

    Configure Persistence Layer: UnitOfWork, RavenDb configuration, ContractResolver

---
+++ src/Persistence/SpendSmart.Persistence/Options/RavenDbOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace SpendSmart.Persistence.Options;
+
+/// <summary>
+/// Represents the RavenDB settings.
+/// </summary>
+public sealed class RavenDbOptions
+{
+    /// <summary>
+    /// Gets the certificate.
+    /// </summary>
+    public string Certificate { get; init; }
+
+    /// <summary>
+    /// Gets the database name.
+    /// </summary>
+    public string Database { get; init; }
+
+    /// <summary>
+    /// Gets the cluster nodes URLs.
+    /// </summary>
+    public string[] Urls { get; init; }
+}
