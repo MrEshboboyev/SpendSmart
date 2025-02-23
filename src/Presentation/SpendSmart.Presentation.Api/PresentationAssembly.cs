89267273412524ae5279ca46d85c5802df85c0f8
MrEshbobotev <mreshboboyev@gmail.com>
2025-02-23 15:10:11 +0500

    Default for Presentation

---
+++ src/Presentation/SpendSmart.Presentation.Api/PresentationAssembly.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace SpendSmart.Presentation.Api;
+
+/// <summary>
+/// Represents the presentation assembly.
+/// </summary>
+public static class PresentationAssembly
+{
+    /// <summary>
+    /// The presentation assembly.
+    /// </summary>
+    public static readonly Assembly Assembly = Assembly.GetExecutingAssembly();
+}
